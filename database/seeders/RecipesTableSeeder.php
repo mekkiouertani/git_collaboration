1ee81a25722f72134c5c71b88cb0f63686b470c6
Trotteriniku <stefanozidda3@gmail.com>
2023-12-22 12:58:03 +0100

    added Recipes seeder

---
+++ database/seeders/RecipesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Recipe;
+
+class RecipesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $recipes = config('db.recipes');
+
+    }
+}
